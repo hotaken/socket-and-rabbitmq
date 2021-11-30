8618c47eecaa652fefff9bede3901e730cee885b
hotaken <hotaken@MacBook-Pro-hotaken.local>
2021-11-30 08:07:41 +0500

    2fix

--- client.py
+++ client.py
@@ -4,11 +4,13 @@ import socket_client
 
 if __name__ == "__main__":
     if settings.MTYPE == "sockets":
+        print("Sockets Client")
         client = socket_client.SocketClient()
         client.set_up()
 
         client.start()
     else:
+        print("RabbitMQ Client")
         try:
             client = rmq_client.RMQClient()
             client.start()
